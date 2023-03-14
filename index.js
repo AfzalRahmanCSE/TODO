1d3caa6bd3c1512f0f326249a1bdf995ddd91339
AfzalUrRahman <afzalrahman.cse@gmail.com>
2023-03-14 12:00:38 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const {NODE_ENV='production'}=process.env
+
+require('dotenv').config({
+    path:`.${NODE_ENV}.env`
+})
+
+const express=require('express')
+const app=express()
+
+app.get('/',(req,res)=>{
+    res.status(200)
+    res.send('Server is up and Running')
+})
+const port=process.env.port;
+
+app.listen(port,()=>{
+    console.log(`Server running on port ${port}...`)
+})
